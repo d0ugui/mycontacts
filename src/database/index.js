1af5b3bdee4fe2e87a8fa3c723bd8a87575cdc20
Douglas Oliveira <douglaspo_97@outlook.com>
2021-12-22 16:38:13 -0300

    conect to database and create tables

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg');
+
+// Objeto contendo as infos de conexão e acesso
+const client = new Client({
+  host: 'localhost',
+  port: 5432,
+  user: 'root',
+  password: 'root',
+  database: 'mycontacts',
+});
+
+// Efetuando conexão
+client.connect();
+
+// Função de query
+exports.query = async (query) => {
+  const { rows } = await client.query(query);
+  return rows;
+};
