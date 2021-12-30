e96fb6d1b706dc0c72e31d91de2081c185efb5d0
Douglas Oliveira <douglaspo_97@outlook.com>
2021-12-29 21:52:03 -0300

    creating contactslist-listContainer

--- front-end/src/components/ContactsList/index.js
+++ front-end/src/components/ContactsList/index.js
@@ -1,4 +1,10 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export function ContactsList() {
   return (
@@ -7,6 +13,75 @@ export function ContactsList() {
         <strong>3 contatos</strong>
         <a href="/">Novo contato</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Douglas Oliveira</strong>
+              <small>instagram</small>
+            </div>
+            <span>douglaspo_97@outlook.com</span>
+            <span>(11) 99999-2222</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Douglas Oliveira</strong>
+              <small>instagram</small>
+            </div>
+            <span>douglaspo_97@outlook.com</span>
+            <span>(11) 99999-2222</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Douglas Oliveira</strong>
+              <small>instagram</small>
+            </div>
+            <span>douglaspo_97@outlook.com</span>
+            <span>(11) 99999-2222</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
     </Container>
   );
 }
