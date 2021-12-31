a2bc747bce274d233628e165ff6d9a0ac5cde8a6
Douglas Oliveira <douglaspo_97@outlook.com>
2021-12-31 20:07:57 -0300

    creating input and select components

--- front-end/src/pages/NewContact/index.js
+++ front-end/src/pages/NewContact/index.js
@@ -1,9 +1,17 @@
 import { PageHeader } from '../../components/PageHeader';
+import { Input } from '../../components/Input';
+import { Select } from '../../components/Select';
 
 // import { Container } from './styles';
 
 export function NewContact() {
   return (
-    <PageHeader title="Novo contato" />
+    <>
+      <PageHeader title="Novo contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+      </Select>
+    </>
   );
 }
