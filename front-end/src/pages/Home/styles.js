3ac8e33792c2cd904ccebc10bd575d3a0ae93d36
Douglas Oliveira <douglaspo_97@outlook.com>
2021-12-30 21:43:43 -0300

    creating homepage and refactoring header component

--- front-end/src/pages/Home/styles.js
+++ front-end/src/pages/Home/styles.js
@@ -4,12 +4,32 @@ export const Container = styled.div`
   margin-top: 32px;
 `;
 
+export const InputSearchContainer = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    background: #fff;
+    border: none;
+    border-radius: 25px;
+    height: 50px;
+    box-shadow: 0px 4px 10px rgba(0,0,0,0.04);
+    outline: none;
+    padding: 0 16px;
+
+    &::placeholder {
+      color: #BCBCBC;
+    }
+  }
+`;
+
 export const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
   padding-bottom: 16px;
   border-bottom: 2px solid rgba(188, 188, 188, 0.2);
+  margin-top: 32px;
 
   strong {
     font-size: 24px;
