9adcbbd7975701d2941f75f1a8d948b603bb4955
Douglas Oliveira <douglaspo_97@outlook.com>
2022-01-02 19:35:17 -0300

    add danger status in modal

--- front-end/src/components/Modal/index.js
+++ front-end/src/components/Modal/index.js
@@ -1,11 +1,13 @@
+import PropTypes from 'prop-types';
+
 import { Button } from '../Button';
 
 import { Overlay, Container, Footer } from './styles';
 
-export function Modal() {
+export function Modal({ danger }) {
   return (
     <Overlay>
-      <Container>
+      <Container danger={danger}>
         <h1>Título do modal</h1>
         <p>
           Corpo do modal
@@ -15,7 +17,7 @@ export function Modal() {
           <button type="button" className="cancel-button">
             Cancelar
           </button>
-          <Button type="button">
+          <Button type="button" danger={danger}>
             Deletar
           </Button>
         </Footer>
@@ -23,3 +25,11 @@ export function Modal() {
     </Overlay>
   );
 }
+
+Modal.propTypes = {
+  danger: PropTypes.bool,
+};
+
+Modal.defaultProps = {
+  danger: false,
+};
