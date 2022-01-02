2a051f5784407438a212a6606c6eb752e0381e6e
Douglas Oliveira <douglaspo_97@outlook.com>
2022-01-01 22:27:37 -0300

    creating button component

--- front-end/src/pages/NewContact/index.js
+++ front-end/src/pages/NewContact/index.js
@@ -1,6 +1,7 @@
 import { PageHeader } from '../../components/PageHeader';
 import { Input } from '../../components/Input';
 import { Select } from '../../components/Select';
+import { Button } from '../../components/Button';
 
 // import { Container } from './styles';
 
@@ -12,6 +13,12 @@ export function NewContact() {
       <Select>
         <option value="123">Instagram</option>
       </Select>
+      <Button type="button">
+        Salvar Contato
+      </Button>
+      <Button type="button" disabled>
+        Salvar Contato
+      </Button>
     </>
   );
 }
