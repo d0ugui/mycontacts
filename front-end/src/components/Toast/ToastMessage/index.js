b54eeebde244c14b6ef0fbd992bc8eaef773d7bc
Douglas Oliveira <douglaspo_97@outlook.com>
2023-09-20 21:35:52 -0300

    refactor: add memo in toastMessage

--- front-end/src/components/Toast/ToastMessage/index.js
+++ front-end/src/components/Toast/ToastMessage/index.js
@@ -1,11 +1,11 @@
-import { useEffect } from 'react';
+import { memo, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { Container } from './styles';
 import xCircleIcon from '../../../assets/images/icons/x-circle.svg';
 import checkCircleIcon from '../../../assets/images/icons/check-circle.svg';
 
-export default function ToastMessage({
+function ToastMessage({
   message, onRemoveMessage, isLeaving, animatedRef,
 }) {
   useEffect(() => {
@@ -49,3 +49,5 @@ ToastMessage.propTypes = {
   isLeaving: PropTypes.bool.isRequired,
   animatedRef: PropTypes.shape().isRequired,
 };
+
+export default memo(ToastMessage);
